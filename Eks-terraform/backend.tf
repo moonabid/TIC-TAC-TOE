c54b6ed9d9987b879ebd2972c2bb3819e8e23047
ABID Moon <60834531+moonabid@users.noreply.github.com>
2023-11-05 18:05:19 +0000

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "moon-storage" # Replace with your actual S3 bucket name
+    bucket = "moon-storage23" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
